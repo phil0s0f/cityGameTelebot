4d8e60520ff38ecac95dc62d8843606bc179acc1
Дугаров Вячеслав <sdugharow@yandex.ru>
2024-12-16 14:09:45 +0800

    init

---
+++ CitiesGame.py
@@ -0,0 +1,49 @@
+# CitiesGame
+from CitiesHistory import CitiesHistory
+from llm_city import is_valid_city_name, get_city_name
+
+
+class CitiesGame:
+    def __init__(self):
+        self.chat_dict = {}
+
+    def turn(self, chat_id, user_input):
+        cities_history = self.chat_dict.get(chat_id, CitiesHistory())
+
+        if not is_valid_city_name(user_input):
+            return f'Города "{user_input}" не существует.', False
+
+        try:
+            cities_history.add_city(user_input)
+        except ValueError as e:
+            return str(e), False
+
+        start_letter = cities_history.get_last_valid_letter()
+        excluded_cities = cities_history.get_cities_by_letter(start_letter)
+
+        next_city = get_city_name(start_letter, excluded_cities)
+
+        if not next_city:
+            return self.__bot_give_up(chat_id), False
+
+        try:
+            cities_history.add_city(next_city)
+        except ValueError as e:
+            print(str(e))
+            return self.__bot_give_up(chat_id), False
+
+        self.chat_dict[chat_id] = cities_history
+        print(f"Список названных городов в чате {chat_id}: {cities_history}")
+        return next_city, True
+
+    def give_up(self, chat_id):
+        self.chat_dict.pop(chat_id, None)
+        return
+
+    def history(self, chat_id):
+        return self.chat_dict.get(chat_id, CitiesHistory()).get_history()
+
+    def __bot_give_up(self, chat_id):
+        print(f"Бот сдался в чате: {chat_id}")
+        self.chat_dict.pop(chat_id, None)
+        return "Я сдаюсь! В этот раз ты победил."
---
+++ voice_processing.py
@@ -0,0 +1,41 @@
+# voice_processing
+import speech_recognition as sr
+import soundfile as sf
+from gtts import gTTS
+import os
+
+
+def voice_to_text(message_id, ogg_file):
+    """Преобразование голоса в текст"""
+    # Конвертация OGG в WAV
+    wav_file = f"{message_id}_voice.wav"
+    data, samplerate = sf.read(ogg_file)
+    sf.write(wav_file, data, samplerate)
+
+    # Преобразование голоса в текст
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(wav_file) as source:
+        audio_data = recognizer.record(source)
+        try:
+            text = recognizer.recognize_google(audio_data, language="ru-RU")
+            print(f"Распознанный текст: {text}")
+            # bot.reply_to(message, f"Распознанный текст: {text}")
+        except sr.UnknownValueError:
+            text = "Не удалось распознать речь."
+        except sr.RequestError as e:
+            text = f"Ошибка распознавания: {e}"
+    # Удаление временных файлов
+    os.remove(ogg_file)
+    os.remove(wav_file)
+
+    return text
+
+
+def text_to_voice(message_id, text):
+    """Преобразование текста в голос"""
+    tts = gTTS(text=text, lang="ru")
+    output_voice_file = f"{message_id}_response.mp3"
+    tts.save(output_voice_file)
+
+    return output_voice_file
+
